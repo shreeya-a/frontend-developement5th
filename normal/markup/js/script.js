247f8ec0a64a51912137a7b9d0cb88ef99a88ee1
shreeya-a <shreeyadhikari11@gmail.com>
2022-08-09 09:00:41 +0545

    feedback-slider

--- normal/markup/js/script.js
+++ normal/markup/js/script.js
@@ -4,37 +4,52 @@
 
 
 jQuery('.feedback-slider').slick({
-    dots: true,
+    dots: false,
     infinite: false,
     speed: 300,
-    slidesToShow: 4,
-    slidesToScroll: 4,
+    mobileFirst:true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+
+    prevArrow: '<button class="slick-prev" aria-label="Previous" type="button"><i class="icon-prev"></i></button>',
+    nextArrow: '<button class="slick-next" aria-label="Next" type="button"><i class="icon-next"></i></button>',
+    
     responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          //  slidesToScroll: 2,
+        }
+      },
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
+          slidesToShow: 1,
+          // slidesToScroll: 2
         }
       },
       {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
-        breakpoint: 480,
+        breakpoint: 1200,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
+          // slidesToScroll: 1
         }
       }
       // You can unslick at a given breakpoint now by adding:
       // settings: "unslick"
       // instead of a settings object
     ]
+  });
+  jQuery('.banner-slider').slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows:false,
+    prevArrow: '<button class="slick-prev" aria-label="Previous" type="button"><i class="icon-prev"></i></button>',
+    nextArrow: '<button class="slick-next" aria-label="Next" type="button"><i class="icon-next"></i></button>',
+    
+ 
   });
